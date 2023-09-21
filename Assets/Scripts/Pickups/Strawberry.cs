969a7e9be94e53ed4386a87246e025d499627afd
Wumpie7 <chrisjaver@gmail.com>
2023-09-22 01:29:24 +0200

    Added strawberry pick up sound and adjust player

--- Assets/Scripts/Pickups/Strawberry.cs
+++ Assets/Scripts/Pickups/Strawberry.cs
@@ -1,13 +1,36 @@
 using UnityEngine;
 using System;
+using System.Collections;
 
 public class Strawberry : MonoBehaviour
 {
     public static Action onPickUp;
+    private new AudioSource audio;
+    private bool started;
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void Start()
     {
+        audio = GetComponent<AudioSource>();
+        audio.volume = 0.1f;
+    }
+
+    private IEnumerator HandleCollision() 
+    {
+        if (started) yield break;
+        
         onPickUp?.Invoke();
+
+        audio.Play();
+
+        started = true;
+
+        GetComponent<SpriteRenderer>().enabled = false;
+        GetComponent<BoxCollider2D>().enabled = false;
+
+        yield return new WaitWhile(() => audio.isPlaying);
+
         Destroy(gameObject);
     }
+
+    private void OnCollisionEnter2D() => StartCoroutine(HandleCollision());
 }
