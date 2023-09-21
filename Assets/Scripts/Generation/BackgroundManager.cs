a097c7a7f436037262b0eda0f8e44be531d0792f
Wumpie7 <chrisjaver@gmail.com>
2023-09-21 17:26:34 +0200

    Added moving backgrounds, some sprites are gaping

--- Assets/Scripts/Generation/BackgroundManager.cs
+++ Assets/Scripts/Generation/BackgroundManager.cs
@@ -1,13 +1,43 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class BackgroundManager : MonoBehaviour
 {
-    [SerializeField] private Sprite[] backgrounds;
+    private Vector3 scrollSpeed = new(-0.01f, 0, 0);
+    [SerializeField] private SpriteRenderer[] renderers;
+    [SerializeField] private Sprite[] bgs;
+    private int bgIndex, bgTransitionCount;
+    private readonly int bgTransitionTreshold = 5;
+    private const int parent = 0, child = 1;
 
-    void Update()
+    private void UpdateBackground()
     {
-        
+        foreach (SpriteRenderer renderer in renderers)
+            renderer.sprite = bgs[bgIndex];
+        renderers[child].transform.position = Vector3.zero;
+        renderers[child].transform.position += new Vector3(renderers[parent].size.x, 0);
+    }
+
+    private void ChangeBackground()
+    {
+        bgIndex = (bgIndex + 1) % bgs.Length;
+        UpdateBackground();
+    }
+
+    private void Start() => UpdateBackground();
+
+    private void Update()
+    {
+        transform.position += scrollSpeed;
+
+        if (transform.position.x * -1 >= renderers[parent].size.x)
+        {
+            transform.position = Vector3.zero;
+            bgTransitionCount++;
+        }
+        else if (bgTransitionCount == bgTransitionTreshold)
+        {
+            ChangeBackground();
+            bgTransitionCount = 0;
+        }
     }
 }
