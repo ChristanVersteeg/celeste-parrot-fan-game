fc506a798b045b1a5026f5c4e937cfa784616efb
Wumpie7 <chrisjaver@gmail.com>
2023-09-22 19:37:56 +0200

    Fixed scenes switching by collision whilst in other scenes

--- Assets/Scripts/Player/Collision.cs
+++ Assets/Scripts/Player/Collision.cs
@@ -6,7 +6,9 @@ using UnityEngine.SceneManagement;
 public class Collision : MonoBehaviour
 {
     private void OnCollisionEnter2D(Collision2D collision)
-    { 
+    {
+        if (SceneManager.GetActiveScene().buildIndex != 1) return;
+
         if(collision.gameObject.name != Strawberry.name)
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
