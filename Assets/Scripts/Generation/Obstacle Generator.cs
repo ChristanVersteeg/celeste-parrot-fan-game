7905fd1f5f6ffc5e6f191f79aaa2bc03c6f42a1f
Terdirk12 <71409021+Terdirk12@users.noreply.github.com>
2023-09-22 02:47:58 +0200

    finalized obstacles

--- Assets/Scripts/Generation/Obstacle Generator.cs
+++ Assets/Scripts/Generation/Obstacle Generator.cs
@@ -5,16 +5,33 @@ using UnityEngine;
 
 public class ObstacleGenerator : MonoBehaviour
 {
-    public List<GameObject> obstacleList;
-    float timer, spawnTime;
-
-    void Update()
+    public List<GameObject> bottomObstacleList;
+    public List<GameObject> topObstacleList;
+    public Transform topSpawner, bottomSpawner;
+    private int incrementor = 0;
+    WaitForSeconds spawnTime = new(2.0f);
+    public void Start()
     {
-        timer += Time.deltaTime;
-        if (timer > (spawnTime - Time.deltaTime))
-        {
-            timer = 0;
-            Instantiate(obstacleList[Random.Range(0, obstacleList.Count)], this.transform.position, Quaternion.identity);
+        StartCoroutine(Spawner());
+    }
+
+    private IEnumerator Spawner()
+    {
+        while (true)
+        {          
+            if (incrementor % 2 == 0)
+            {
+                Instantiate(bottomObstacleList[Random.Range(0, bottomObstacleList.Count)], bottomSpawner.position, Quaternion.identity);
+                incrementor++;
+            }
+            else
+            {         
+                Instantiate(topObstacleList[Random.Range(0, topObstacleList.Count)], topSpawner.position, Quaternion.identity);
+                incrementor++;
+            }
+
+            yield return spawnTime;
         }
     }
 }
+
