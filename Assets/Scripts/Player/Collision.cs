12263f06f5653e993c342b21cd4533a95c2a5bdd
Terdirk12 <71409021+Terdirk12@users.noreply.github.com>
2023-09-22 20:52:16 +0200

    updated strawberry collision

--- Assets/Scripts/Player/Collision.cs
+++ Assets/Scripts/Player/Collision.cs
@@ -6,8 +6,8 @@ using UnityEngine.SceneManagement;
 public class Collision : MonoBehaviour
 {
     private void OnCollisionEnter2D(Collision2D collision)
-    { 
-        if(collision.gameObject.name != Strawberry.name)
+    {
+        if (collision.gameObject.CompareTag("Strawberry")) return;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
