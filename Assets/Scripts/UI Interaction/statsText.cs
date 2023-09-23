51d5e91e05dbb561284c584e3aed942a7dccbbfe
Terdirk12 <71409021+Terdirk12@users.noreply.github.com>
2023-09-23 22:15:17 +0200

    qaaaaaaahahahahah

--- Assets/Scripts/UI Interaction/statsText.cs
+++ Assets/Scripts/UI Interaction/statsText.cs
@@ -3,12 +3,17 @@ using TMPro;
 public class DisplayStats : MonoBehaviour
 {
     public TextMeshProUGUI statsText; // Reference to the TextMeshPro Text for displaying stats
+    int totalStrawberries;
+
+    private void FixedUpdate()
+    {
+        totalStrawberries = PlayerPrefs.GetInt("totalStrawberries", 0);
+    }
 
     void Start()
     {
         // Retrieve values from PlayerPrefs
         int totalDeaths = PlayerPrefs.GetInt("totalDeaths", 0);
-        int totalStrawberries = PlayerPrefs.GetInt("totalStrawberries", 0);
 
         // Create a formatted string to display the stats
         string statsString = "Stats:\nTotal Strawberries: " + totalStrawberries + "\nTotal Deaths: " + totalDeaths;
