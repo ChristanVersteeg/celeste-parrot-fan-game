ea1123532f84284efc1373557c62952700f3e760
Terdirk12 <71409021+Terdirk12@users.noreply.github.com>
2023-09-24 17:55:30 +0200

    new level and vertical movement

--- Assets/Scripts/Generation/GridMovement.cs
+++ Assets/Scripts/Generation/GridMovement.cs
@@ -2,10 +2,14 @@ using UnityEngine;
 
 public class GridMovement : MonoBehaviour
 {
-    public static Vector3 scrollSpeed = new(-0.1f, 0, 0);
+    public static Vector3 scrollSpeed = new(0, 0, 0);
 
     private void FixedUpdate()
     {
+        if (LevelManager.moveVertical && LevelManager.moveHorizontal) scrollSpeed = new(-0.1f, 0.1f, 0);
+        else if(LevelManager.moveHorizontal) scrollSpeed = new(-0.1f, 0, 0);
+        else if (LevelManager.moveVertical) scrollSpeed = new(0, -0.05f, 0);
+
         if (!LevelManager.moveGrid) return;
         transform.position += scrollSpeed;
     }
