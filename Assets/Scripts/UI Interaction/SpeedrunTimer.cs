2c54dfc05ef86d46f5359bedfca8af182e2cd210
Wumpie7 <christan.versteeg@hva.nl>
2023-10-11 13:27:29 +0200

    Added speedrun timer

---
+++ Assets/Scripts/UI Interaction/SpeedrunTimer.cs
@@ -0,0 +1,30 @@
+using System;
+using TMPro;
+using UnityEngine;
+
+public class SpeedrunTimer : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI currentTime, overallTime;
+    private float time;
+
+    private void OnEnable()
+    {
+        time = PlayerPrefs.GetFloat(nameof(time));
+    }
+
+    private void OnDisable()
+    {
+        PlayerPrefs.SetFloat(nameof(time), time + Time.timeSinceLevelLoad);
+    }
+
+    void Update()
+    {
+        TimeSpan currentTimeSpan = TimeSpan.FromSeconds(Time.timeSinceLevelLoad);
+        string currentTimeText = $"{currentTimeSpan.Hours}:{currentTimeSpan.Minutes:00}:{currentTimeSpan.Seconds:00}<size=80%><color=#808080>.{currentTimeSpan.Milliseconds:000}</color></size>";
+        currentTime.text = currentTimeText;
+
+        TimeSpan overallTimeSpan = TimeSpan.FromSeconds(time + Time.timeSinceLevelLoad);
+        string overallTimeText = $"{overallTimeSpan.Minutes:00}:{overallTimeSpan.Seconds:00}";
+        overallTime.text = overallTimeText;
+    }
+}
